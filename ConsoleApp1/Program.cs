41bbf263e7e4b66cdfd7a8c7215bdbe75df7ede9
visalatchi.annadurai <visalatchi.annadurai@CESIT-LAP-0384>
2022-10-25 15:27:07 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+
+public class Exercise
+{
+    public static void Main()
+
+    //static void Main(string[] args)
+
+    //{
+    //int[] first_array = { 1, 2, 3, 4, };
+    //int[] second_array = { 1, 2, 3, 4, };
+    //Console.WriteLine("Array1:" + first_array);
+    //Console.WriteLine("Array 2:" + second_array);
+    //Console.WriteLine("first_array * second_array");
+    //for(int i = 0; i < first_array.Length; i++)
+    //{
+    // Console.WriteLine(first_array[i]* second_array[i]);
+    //}
+
+    //Console.WriteLine("Enter first integer:");
+    //int x= Convert.ToInt32(Console.ReadLine()); 
+    //if(x>0)
+    //{
+    //  Console.WriteLine(x%3==0 || x%7==0);
+    //}
+    //}
+
+
+    //find largest and smalleat from given three integers
+    //Console.WriteLine("Enter first number:");
+    //  int a = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("Enter second number:");
+    // int b = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Enter third number:");
+    // int c = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Highest number:" + Math.Max(a, Math.Max(b, c)));
+    // Console.WriteLine("Lowest number:" + Math.Min(b, Math.Min(c, a)));
+    //{
+    //    Console.WriteLine("Enter first integer:");
+    //    int a = Convert.ToInt32(Console.ReadLine());
+    //    Console.WriteLine("Enter second integer:");
+    //    int b = Convert.ToInt32(Console.ReadLine());
+
+
+    //program to create a new string where the first 4 characters will be in lower case. If the string is less than 4 characters then make the whole string in upper case
+    // string Name = "visalatchi";
+
+    //if(Name.Length<4)
+    // {
+    //     Console.WriteLine(Name.ToUpper());
+    // }
+    // else
+    // {
+    //     Console.WriteLine(Name.ToLower());
+    // }
+    //program to count a specified number in a given array of integers.
+
+
+    //{
+    //    Console.WriteLine("Enter the integer:");
+    //    int x = Convert.ToInt32(Console.ReadLine());
+    //    int[] nums = { 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6 };
+    //    Console.WriteLine("Number of" +x+ "present in the array");
+    //    Console.WriteLine(nums.Count(nums => nums==x));
+    //}
+    //program to check if a number appears as either the first or last element of an array of integers and the length is 1 or more
+    //{
+
+    //    Console.WriteLine("Enter a integer:");
+    //    int x= Convert.ToInt32(Console.ReadLine());
+    //    int[] nums = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    //    Console.WriteLine(nums[0]==x || nums[nums.Length-1] ==x);
+
+    //}
+    // program to check if the first element or the last element of the two arrays ( length 1 or more) are equal.
+    //    {
+    //        int[] num1 = { 1, 2, 3, 4, 5, 6, };
+    //        Console.WriteLine("Array 1 [{0}]", string.Join(", ", num1));
+    //        int[] num2 = { 1, 2, 3, 4, 5, 6, };
+    //        Console.WriteLine("Array 2 [{0}] ", string.Join(", ", num2));
+
+    //        Console.WriteLine((num1[0].Equals(num2[0])) || (num1[num1.Length - 1].Equals(num2[num2.Length - 1])));
+
+
+
+    //    }
+    //}
+    //program to create a new array of length containing the middle elements of three arrays (each length 3) of integers.
+    //{
+    //    int[] num1 = { 1, 2, 3 };
+    //    Console.WriteLine("Array 1 [{0}]", string.Join(", ", num1));
+    //    int[] num2 = { 1, 0, 3 };
+    //    Console.WriteLine("Array 2 [{0}]", string.Join(",", num2));
+    //    int[] num3 = { 1, 9, 0 };
+    //    Console.WriteLine("Array 3 [{0}]", string.Join(",", num3));
+    //    int[] num4 = { num1[1], num2[1], num3[1]};
+    //    Console.WriteLine("Output [{0}]", string.Join(",", num4));
+
+    //}
+    {
+
+    }
+}
+
+       
+   
+    
+
+
+
+    
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
